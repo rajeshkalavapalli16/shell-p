ad377edeab5e0d27b097b6766187578e76380780
rajeshkalavapalli <rajesh.kalavapalli@gmail.com>
2024-03-24 20:06:33 +0530

    first task finishe modified

---
+++ User_Input.sh
@@ -0,0 +1,10 @@
+# Task 2: User Input
+#Create a script that prompts the user to enter their name, then outputs a greeting using that name.
+
+
+#!/bin/bash 
+
+echo "please enter your name "
+read name 
+
+echo "your name is $name"
