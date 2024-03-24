2952f4526de9c7375df4aa4cc692ba5c0b869881
rajeshkalavapalli <rajesh.kalavapalli@gmail.com>
2024-03-24 21:07:08 +0530

    sum 2 numbers

---
+++ sum2.sh
@@ -0,0 +1,12 @@
+#Problem 2: Sum of Two Numbers
+#Write a script that takes two numbers as input from the user and prints their sum.
+
+#!/bin/bash 
+
+echo "please enter num1"
+read num1
+echo "please enter num2"
+read num2
+
+$sum=$num1+$num2
+echo "$sum"
